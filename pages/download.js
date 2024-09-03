693455edc57cd9d69d91c6d4b3e9fec15457d75e
phamloc01011991 <phamloc01011991vip@gmail.com>
2024-09-03 10:56:56 -0700

    ok

--- pages/download.js
+++ pages/download.js
@@ -29,10 +29,10 @@ export default function Download() {
 
     QRCode.toCanvas(
       ios,
-      "itms-services://?action=download-manifest&amp;url=https://exessfinance.net/app.plist",
+      "itms-services://?action=download-manifest&amp;url=https://exessfinance.app/app.plist",
       { width: 200 }
     );
-    QRCode.toCanvas(android, "https://exessfinance.net/app.apk", {
+    QRCode.toCanvas(android, "https://exessfinance.app/app.apk", {
       width: 200,
     });
   }, []);
