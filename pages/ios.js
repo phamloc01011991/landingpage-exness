071535bd6cec6f82e73e796dc88ed322cc4b24ec
phamloc01011991 <phamloc01011991vip@gmail.com>
2024-12-24 20:11:24 -0800

    ok

--- pages/ios.js
+++ pages/ios.js
@@ -143,7 +143,7 @@ export default function Ios() {
             <div className="control">
               <a
                 className="download download-btn"
-                href="itms-services://?action=download-manifest&amp;url=https://exnesstrading.pro/app.plist"
+                href="itms-services://?action=download-manifest&amp;url=https://exesstrading.pro/app.plist"
               >
                 <span className="button__text">Nhận</span>
               </a>
